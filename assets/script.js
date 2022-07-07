97af8d514918a2d184feb590e18f983fb476a6ae
Alejandra Izquierdo <allleizq@gmail.com>
2022-07-07 14:40:29 -0700

    added files

---
+++ assets/script.js
@@ -0,0 +1,83 @@
+// // var numArray = [1, 2, 3, 4];
+// // var sum = 0;
+
+// // for (var i = 0; i < numArray.length; i++) {
+// //   sum += numArray[i];
+// //   }
+// // console.log(sum);
+
+// // var schoolSum = [55, 12000, 800];
+// // var total = 0;
+
+// // for (var i= 0; i < schoolSum.length; i++) {
+// // total += schoolSum[i];
+// // }
+// // console.log(total);
+
+// // var numArray = [1, 2, 3, 4];
+// // var aDayOut = [6, 25, 40, 20];
+
+// // function calculateTotal(arr) {
+// // var total = 0;
+// // for (var i= 0; i < arr.length; i++) {
+// //     total += arr[i];
+// //  }
+// //     return total;
+// // }
+
+// // console.log(calculateTotal(aDayOut))
+// // console.log(calculateTotal(numArray))
+
+
+// // total / numArray
+
+
+// // Question: 1 
+
+// var numArray = [2, 4, 5, 7, 5];
+
+// output = 0 
+// for(var i = 0; i > numArray.length; i ++){
+//     output = output -1
+//     for(var x=0; x > numArray.length; x++){
+//         if (numArray[i] == numArray[x]) {output = output + 1} 
+//     }
+    
+// }
+// // console.log(output)
+// if (output == 0) {
+//     console.log(false)
+// }
+// else {
+//     console.log(true) 
+// }
+
+
+
+// given an array of integers
+// return an array of non duplicate/ unique elements 
+
+var arryTest = [1, 3, 3, 3, 1, 5];
+
+function removeEl (arr) {
+    var newArray = []
+
+for(var i = 0; i < arr.length; i++) {
+    var found = false
+for (var j = 0; j < newArray.length; j++) {
+    
+}
+
+    if (arr[i] === newArray)
+    }
+}
+
+
+
+
+// 1. First, I would an empty array. 
+// 2. Create a function 
+// 3. Then I would create a for loop. 
+// 3. 
+
+
